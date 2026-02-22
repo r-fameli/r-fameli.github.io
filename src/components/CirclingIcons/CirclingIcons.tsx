920cfb0fa6922707947f65763dbe3ec5b9e9c3d3
r-fameli <rikifameli@gmail.com>
2026-02-21 16:46:12 -0800

    Update CirclingIcons to move them down and format/clean file slightly

--- src/components/CirclingIcons/CirclingIcons.tsx
+++ src/components/CirclingIcons/CirclingIcons.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import './CirclingIconsIntro.scss';
-import { useAnimationFrame } from '../../hooks/useAnimationFrame';
+import { useState } from "react";
+import "./CirclingIconsIntro.scss";
+import { useAnimationFrame } from "../../hooks/useAnimationFrame";
 
-const radians = (degrees: number) => degrees * (Math.PI / 180)
+const radians = (degrees: number) => degrees * (Math.PI / 180);
 
 type Props = {
-    numCircles: number,
-}
+    numCircles: number;
+};
 
 const CirclingIcons = ({ numCircles }: Props) => {
     // Placing Constants, in cqw and degrees
@@ -30,8 +30,10 @@ const CirclingIcons = ({ numCircles }: Props) => {
         for (let i = 0; i < numCircles; i++) {
             const angle = (offset + angleSteps * i) % 360;
 
-            const xBeforeRot = (DIST_TO_CENTER + DIST_X_OFFSET) * Math.cos(radians(angle));
-            const yBeforeRot = (DIST_TO_CENTER + DIST_Y_OFFSET) * Math.sin(radians(angle));
+            const xBeforeRot =
+                (DIST_TO_CENTER + DIST_X_OFFSET) * Math.cos(radians(angle));
+            const yBeforeRot =
+                (DIST_TO_CENTER + DIST_Y_OFFSET) * Math.sin(radians(angle));
 
             // Apply rotation to tilt the moons on an axis
             const cosRot = Math.cos(rotationOffsetRadians);
@@ -40,26 +42,28 @@ const CirclingIcons = ({ numCircles }: Props) => {
             const y = CENTER + (xBeforeRot * sinRot + yBeforeRot * cosRot);
             // z-index
             const z = angle < 180 ? 5 : -1;
-            positions[i] = [x, y, z]
-        };
-        return positions
-    }
+            positions[i] = [x, y, z];
+        }
+        return positions;
+    };
 
     useAnimationFrame((deltaTime: number) => {
         if (Math.abs(velocity) > Math.abs(VELOCITY_BASE)) {
-            setVelocity(velocity => velocity * 0.99 * (deltaTime * 0.001));
+            setVelocity((velocity) => velocity * 0.99 * (deltaTime * 0.001));
         } else if (velocity < Math.abs(VELOCITY_BASE)) {
             setVelocity(VELOCITY_BASE);
         }
         // Adjust the offset of the circles based on time elapsed
-        setAngleOffset(angleOffset => angleOffset + (velocity * deltaTime * 0.001));
-    })
+        setAngleOffset(
+            (angleOffset) => angleOffset + velocity * deltaTime * 0.001,
+        );
+    });
 
-    const circlePositions = getCirclePositions(numCircles, angleOffset)
+    const circlePositions = getCirclePositions(numCircles, angleOffset);
 
-    return <div className="CirclingIcons">
-        <div className="circling-icons-frame">
-            <div className="big-circle"></div>
+    return (
+        <div className="CirclingIcons">
+            <div className="big-circle" />
             <ul className="little-circles">
                 {circlePositions.map(([x, y, zIndex], i) => (
                     <li
@@ -67,13 +71,14 @@ const CirclingIcons = ({ numCircles }: Props) => {
                         style={{
                             // transform: `translate3d(${x}px, ${y}px, 0)`,
                             transform: `translate3d(${x}cqw, ${y}cqw, 0)`,
-                            transition: 'transform 0.1s ease-out',
+                            transition: "transform 0.1s ease-out",
                             zIndex: zIndex,
-                        }}></li>
+                        }}
+                    ></li>
                 ))}
             </ul>
         </div>
-    </div>
-}
+    );
+};
 
 export default CirclingIcons;
