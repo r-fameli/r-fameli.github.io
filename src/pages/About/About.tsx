2551520ffd2456ae6ce942f849c8b1c8218fbd9b
Riki Fameli <rikifameli@gmail.com>
2026-03-19 14:57:09 -0700

    Update about section

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -15,15 +15,15 @@ const About = ({ sentinelRef }: Props) => {
                     <span className="about-text">
                         <h1>about me</h1>
                         <p>
-                            Hi, I'm Riki! I'm a full-stack developer
+                            Hey there! I'm Riki. I'm a full-stack developer
                             previously at Microsoft and a Brown
-                            University alumnus. Much of my experience is in web
-                            development, but I'm willing and able to learn all
-                            sorts of technologies to build clever solutions that feel fluid.
-                            In my work, I'm passionate about making both users and developers happy,
-                            teaching what I've learned to my peers, and making things I'm proud of.
-                            I'm passionate about teaching, user experience, human rights,
-                            and all sorts of physical hobbies ranging from aerial acrobatics to jiu jitsu.
+                            University alumnus. I love building web experiences that
+                            feel fluid and creative, and I'm eager to push the boundaries of what
+                            web/frontend development looks like. In my work, I enjoy making both users
+                            and developers happy and building things
+                            that make others wonder "how did he do that?". I'm also passionate
+                            about teaching others, learning new skills, human rights, and all sorts of physical hobbies
+                            ranging from aerial acrobatics to jiu jitsu.
                         </p>
                     </span>
                 </div>
